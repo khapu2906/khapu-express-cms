acd3d0571ed3c044fe134f1d13bd26b87b885ae9
khapu <daikhanh9260@gmail.com>
2022-05-05 10:18:42 +0700

    update

--- src/commands/init/index.js
+++ src/commands/init/index.js
@@ -1,12 +1,12 @@
 const args = process.argv.slice(2)
 const chalk = require('chalk')
 const fs = require('fs')
-const command = args[0] ?? null
-const typeAndName = args[1] ?? null;
+const command = args[0] ? args[0] : null
+const typeAndName = args[1] ? args[1] : null;
 const path = require('path');
 const APP_PATH = '../../../../../'
 const CORE_PATH = '../../SampleContent/'
-const tagAndPath = args[2] ?? null;
+const tagAndPath = args[2] ? ? null;
 
 let subPath = null
 let type = null
@@ -18,7 +18,7 @@ if (tagAndPath) {
         const tag = arrTagAndPath[0]
         switch (tag) {
             case '--path':
-                subPath = arrTagAndPath[1] ?? null
+                subPath = arrTagAndPath[1] ? arrTagAndPath[1] : null
                 break
             default:
                 console.error(chalk.red('Tag invalid!'))
@@ -46,7 +46,7 @@ if (typeAndName) {
                         if (err) {
                             console.error(chalk.red(`Unsuccessfully create: ${name}!`))
                             return
-                        } 
+                        }
                     })
                 }
                 require('./init')
@@ -57,32 +57,32 @@ if (typeAndName) {
                     if (!type) {
                         console.error(chalk.red('Command invalid!'))
                     } else {
-                        switch(type) {
+                        switch (type) {
                             // create model
-                            case 'model': 
-                                const modelPath = (!subPath) ? path.join(__dirname, APP_PATH, '/app/models/', `${name}.js`) : 
+                            case 'model':
+                                const modelPath = (!subPath) ? path.join(__dirname, APP_PATH, '/app/models/', `${name}.js`) :
                                     path.join(__dirname, APP_PATH, subPath, `${name}.js`)
-                                let contentModelSample = fs.readFileSync(path.join(__dirname, CORE_PATH + 'models/sample' ), 'utf-8')
+                                let contentModelSample = fs.readFileSync(path.join(__dirname, CORE_PATH + 'models/sample'), 'utf-8')
                                 contentModelSample = contentModelSample.replace(/__NAME__/g, name)
                                 fs.writeFileSync(modelPath, contentModelSample, err => {
                                     if (err) {
                                         console.error(chalk.red(`Unsuccessfully create model: ${name}!`))
                                         return
-                                    } 
+                                    }
                                 })
                                 console.log(chalk.green(`Successfully create model: ${name}!`))
                                 break
-                            // create controller
+                                // create controller
                             case 'controller':
-                                const controllerPath = (!subPath) ? path.join(__dirname, APP_PATH, '/app/http/controllers', `${name}.js`) : 
+                                const controllerPath = (!subPath) ? path.join(__dirname, APP_PATH, '/app/http/controllers', `${name}.js`) :
                                     path.join(__dirname, APP_PATH, subPath, `${name}.js`)
-                                let contentControllerSample = fs.readFileSync(path.join(__dirname, CORE_PATH + 'controllers/sample' ), 'utf-8')
+                                let contentControllerSample = fs.readFileSync(path.join(__dirname, CORE_PATH + 'controllers/sample'), 'utf-8')
                                 contentControllerSample = contentControllerSample.replace(/__NAME__/g, name)
                                 fs.writeFileSync(controllerPath, contentControllerSample, err => {
                                     if (err) {
                                         console.error(chalk.red(`Unsuccessfully create controller: ${name}!`))
                                         return
-                                    } 
+                                    }
                                 })
                                 console.log(chalk.green(`Successfully create controller: ${name}!`))
                                 break
